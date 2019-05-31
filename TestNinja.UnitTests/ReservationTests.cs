8db1d9f215248349b8c82adf0405f3148a07f3ef
Pablo Reis <reis.pl1997@gmail.com>
2019-05-31 11:54:14 -0300

    First commit

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCanceleedBy_UserIsAdmin_ReturnsTrue()
+        {
+            var reservation = new Reservation();
+
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCanceleedBy_SameUser_ReturnsTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            var result = reservation.CanBeCancelledBy(user);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCanceleedBy_AnotherUserCancellingReservation_ReturnsFalse()
+        {           
+
+            var reservation = new Reservation { MadeBy = new User()};
+
+            var result = reservation.CanBeCancelledBy(new User());
+
+            Assert.That(result, Is.False); 
+        }
+        
+    }
+}
